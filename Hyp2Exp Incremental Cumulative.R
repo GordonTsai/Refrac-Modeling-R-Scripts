71bcc6d37e5f1a1d2333d3f91638e8590ff3b8b7
GordonTsai <gordon.tsai@na.drillinginfo.com>
2016-12-16 18:25:27 -0600

    Current branch set up for diagnostic plots of oil and gas decline curve streams
    just change the api number

--- Hyp2Exp Incremental Cumulative.R
+++ Hyp2Exp Incremental Cumulative.R
@@ -22,8 +22,13 @@ forecast <- function(input) {
   input = input[order(input$api),]
   input_reduced = subset(input, input$Time2Refrac > 4)
   
+  api = 4200342288
+
+
+  
   #Filter out Refrac data
   refracAPI = unique(input_reduced$api)
+  i = match(api,refracAPI)
   ForecastCumOil = numeric(length(refracAPI))
   #ForecastCumOil_ThreeMonth = numeric(length(refracAPI))
   #ForecastCumOil_SixMonth = numeric(length(refracAPI))
@@ -35,14 +40,22 @@ forecast <- function(input) {
   
   #output = data.frame(api = refracAPI,ForecastCumOil, ForecastCumGas,ForecastCumOil_ThreeMonth,ForecastCumGas_ThreeMonth,ForecastCumOil_SixMonth,ForecastCumGas_SixMonth)
   
-  for(i in 1:length(refracAPI)) {   
+
+
+   #i = 150 #4 is a good one to test
+#  for(i in 1:length(refracAPI)) {   
     count = count+1
     print(count)
-    well = subset(input_reduced, input_reduced$api == 3302501239)
+    well = subset(input_reduced, input_reduced$api == refracAPI[i])
     well$ProductionMonth <- well$ProductionMonth + 1
   
     currentTime = max(well$ProductionMonth)
     well <- well[order(well$ProductionMonth),]
+    originalTime_series = well$ProductionMonth
+    originalProd_series_Oil = well$oilProduction
+    originalProd_series_Gas = well$gasProduction
+    
+    
     well = subset(well, well$ProductionMonth <= well$Time2Refrac[1]) 
     well$oilProduction = well$oilProduction
     well$gasProduction = well$gasProduction
@@ -99,16 +112,27 @@ forecast <- function(input) {
     }  
     
     ##Plot fit
-    #plot(well$rankDate, well$oilProduction, main="Hyperbolic Fit",col="blue", xlab="Time", ylab="Rate") 
-    #lines( well$rankDate,arps.q.hyp2exp(hyp2exp.fit$decline, well$rankDate), col="red")
-    #print(arps.Np.hyp2exp(hyp2exp.fit$decline, currentTime))
+    ##Plotting Oil Two cases zoomed and not zoomed
+    par(mfrow=c(2,2))
+    plot(well$ProductionMonth, well$oilProduction, main="Oil Fit (Zoom)",col="green", xlab="Time", ylab="Rate") 
+    lines(well$ProductionMonth,arps.q.hyp2exp(hyp2exp.fit.Oil$decline, well$ProductionMonth), col="blue")
+    plot(originalTime_series, originalProd_series_Oil, main="Oil Fit",col="green", xlab="Time", ylab="Rate") 
+    lines(well$ProductionMonth,arps.q.hyp2exp(hyp2exp.fit.Oil$decline, well$ProductionMonth), col="blue")
+    print(well$Time2Refrac[1])
+    
+    ##Plotting Gas Two cases zoomed and not zoomed
+    plot(well$ProductionMonth, well$gasProduction, main="Gas Fit (Zoom)",col="red", xlab="Time", ylab="Rate") 
+    lines(well$ProductionMonth,arps.q.hyp2exp(hyp2exp.fit.Gas$decline, well$ProductionMonth), col="blue")
+    plot(originalTime_series, originalProd_series_Gas, main="Gas Fit",col="red", xlab="Time", ylab="Rate") 
+    lines(well$ProductionMonth,arps.q.hyp2exp(hyp2exp.fit.Gas$decline, well$ProductionMonth), col="blue")
+    
     
     ##Output
     ##rateTimeForecast = arps.q.hyp2exp(hyp2exp.fit$decline, currentTimeSeries)
     #CumProduction.Oil = arps.Np.hyp2exp(hyp2exp.fit.Oil$decline, currentTime)
     #CumProduction.Gas = arps.Np.hyp2exp(hyp2exp.fit.Gas$decline, currentTime)
   
-  }
+#  }
   return(output)
 }
 
@@ -128,11 +152,12 @@ hyp2exp.Np <- function (qi, Di, b, Df, t)
   Np
 };
 
+
 output = forecast(input)
 
 #write.table(output, "C:/Users/gordon.tsai/Documents/Refracs/output")
 
-RefracPredictedCumulatives = write.csv(output, "C:/Users/gordon.tsai/Documents/Refracs/output.csv") 
+#RefracPredictedCumulatives = write.csv(output, "C:/Users/gordon.tsai/Documents/Refracs/output.csv") 
 
 #data dump back into SQL ## timeSeries
 #myConn <- odbcDriverConnect('driver={SQL Server};server=gis-mssql.prod.aus,60342;trusted_connection=true')
